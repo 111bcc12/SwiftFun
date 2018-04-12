3f31e8ebdb2579059af244cae2f30ea98757e021
me <me@new2701.local>
2018-04-11 21:03:28 -0500

    Initial Commit

---
+++ SwiftFun/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SwiftFun
+//
+//  Created by me on 4/4/18.
+//  Copyright © 2018 Braden Colt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        view.backgroundColor = UIColor.red
+        
+        myLabel.text = "Hello World"
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
